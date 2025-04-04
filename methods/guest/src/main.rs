d69aa1b11f18ab961efd18a0baad5b5f674f7f32
Vid Kersic <38610409+Vid201@users.noreply.github.com>
2025-04-04 09:48:51 +0200

    Finish partial state update with ssz rs (process_block_header) (#4)
    * feat: progress on fetching beacon chain head and state
    
    * Fetch BeaconState from yaml file
    
    * Define BeamState struct and pass into zkvm guest
    
    * Migrate BeamState up to balances
    
    * process_block_header() through the zkVM
    
    * Use ream package
    
    * Remove rpc code
    
    * Switch to ReamLabs dependencies
    
    * Squat bugs
    
    * Wrong journal output
    
    * partial process block header update without proposer slashing check
    
    * wip: partial state update by adding ssz_rs to ream
    
    * implement ssz_rs on most consensus containers
    
    * almost finish process block header example
    
    * chore: update .gitignore
    
    * feat: git add Cargo.lock files (#5)
    
    * chore: update cargo lock files
    
    * chore: update ssz rs types
    
    * chore: compare data against leaf from proof
    
    * chore: add TODOs
    
    * fix: merge conflicts
    
    ---------
    
    Co-authored-by: unnawut <unnawut@unnawut.com>
    Co-authored-by: Unnawut Leepaisalsuwanna <921194+unnawut@users.noreply.github.com>

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -1,15 +1,66 @@
 use risc0_zkvm::guest::env;
-mod beacon_chain;
+
+use alloy_primitives::FixedBytes;
+use consensus_common::{
+    beacon_block::BeaconBlock,
+    beacon_block_header::BeaconBlockHeader,
+    funcs::{process_block_header, ProcessBlockHeaderOutput},
+    proof::Proof,
+};
+use ssz_rs::prelude::*;
 
 fn main() {
-    // TODO: Implement your guest code here
+    // let count_start = env::cycle_count();
 
-    // read the input
-    let input: u32 = env::read();
+    let pre_state_root: FixedBytes<32> = env::read();
 
-    // TODO: do something with the input
-    let output: u32 = beacon_chain::state_transition::test_passing_slot(input);
+    let slot: u64 = env::read();
+    let slot_proof: Proof = env::read();
+    let latest_block_header_ssz: Vec<u8> = env::read();
+    let latest_block_header: BeaconBlockHeader =
+        BeaconBlockHeader::deserialize(&mut latest_block_header_ssz.as_slice()).unwrap();
+    let latest_block_header_proof: Proof = env::read();
+    // TODO: pass in validator_slashed
+    // let validator_slashed: bool = env::read();
+    // let validator_slashed_proof: Proof = env::read();
 
-    // write public output to the journal
+    let proposer_index: u64 = env::read();
+    // TODO: pass in proposer_index_proof
+
+    let block_ssz: Vec<u8> = env::read();
+    let block = BeaconBlock::deserialize(&mut block_ssz.as_slice()).unwrap();
+
+    // Verify proofs
+    slot_proof.verify().unwrap();
+    assert_eq!(slot_proof.index, 34);
+    assert_eq!(slot_proof.witness, pre_state_root);
+    assert_eq!(slot_proof.leaf, slot.hash_tree_root().unwrap());
+    latest_block_header_proof.verify().unwrap();
+    assert_eq!(latest_block_header_proof.index, 36);
+    assert_eq!(latest_block_header_proof.witness, pre_state_root);
+    assert_eq!(
+        latest_block_header_proof.leaf,
+        latest_block_header.hash_tree_root().unwrap()
+    );
+    // validator_slashed_proof.verify().unwrap();
+    // TODO: assert_eq for generalized index
+    // assert_eq!(validator_slashed_proof.witness, pre_state_root);
+    // assert_eq!(validator_slashed_proof.leaf, validator_slashed.hash_tree_root().unwrap());
+
+    let new_block_header = process_block_header(
+        slot,
+        &latest_block_header,
+        false, // validator_slashed, TODO: fix
+        proposer_index,
+        &block,
+    )
+    .unwrap();
+
+    let output = ProcessBlockHeaderOutput {
+        beacon_block_root: new_block_header.hash_tree_root().unwrap(),
+        new_beacon_block_header_root: new_block_header.hash_tree_root().unwrap(),
+        pre_state_root,
+    };
+
     env::commit(&output);
 }
